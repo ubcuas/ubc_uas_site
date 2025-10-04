5adc2e41e1e547dd8326dae699ab38f2faf36e0a
hixon1000 <harun.sa4@gmail.com>
2025-10-04 13:32:59 -0700

    Initial branch

---
+++ src/mainPage/Front.jsx
@@ -0,0 +1,12 @@
+
+
+function Front() {
+    return (
+        <>
+            <h1>UBC UAS</h1>
+            <h3>UNCREWED AIRCRAFT SYSTEMS</h3>
+            
+        </>
+    )
+}
+export default Front
