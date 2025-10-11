bed75bf820d8d955b22912a7eff4aa19962dda5d
hixon1000 <harun.sa4@gmail.com>
2025-10-11 10:37:59 -0700

    Main page

--- src/mainPage/Front.jsx
+++ src/mainPage/Front.jsx
@@ -1,11 +1,15 @@
-
-
+import './Front.css'
 function Front() {
     return (
         <>
-            <h1>UBC UAS</h1>
-            <h3>UNCREWED AIRCRAFT SYSTEMS</h3>
-            
+        <div className="main">
+            <h1 className="title">UBC UAS</h1>
+            <h3 className="mini-title">UNCREWED AIRCRAFT SYSTEMS</h3>
+            <div className="nav-buttons">
+                <button className="button">Join Us</button>
+                <button className="button">Support Us</button>
+            </div>            
+        </div>
         </>
     )
 }
