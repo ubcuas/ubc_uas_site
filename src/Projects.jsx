cf9a8dc5a83b2bf2d28474466159055dc34242e3
Chaitanya Choudhary <chaitanya.rook@gail.com>
2025-10-11 10:53:01 -0700

    Projects Skeleton

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,15 @@
+import pic_1 from './assets/image 7.png';
 import './Projects.css';
 function Projects() {
 
   return (
-    <div>
+    <>
+    <div className="full-width-image">
       <h1>
         Here's The Projects Page
       </h1>
     </div>
+    </>
   )
 }
 
