42d70d44d83d86e62e2bccba07802e82124c9b3b
hixon1000 <harun.sa4@gmail.com>
2025-10-25 11:41:49 -0700

    Modified the footer to add more info

--- src/footer.jsx
+++ src/footer.jsx
@@ -17,40 +17,44 @@ const Footer = () => {
           <span className="footer__tagline">Uncrewed Aircraft Systems</span>
         </div>
       </div>
-      <nav className="footer__social" aria-label="Social links">
-        <a
-          className="footer__social-link footer__social-link--linkedin"
-          href="https://www.linkedin.com/company/ubcuas"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} alt="Linkedin"/>
-        </a>
-        <a
-          className="footer__social-link footer__social-link--instagram"
-          href="https://www.instagram.com/ubcuas"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={instagram} alt="Instagram"/>
-        </a>
-        <a
-          className="footer__social-link footer__social-link--facebook"
-          href="https://www.facebook.com/ubcuas"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={facebook} alt="Facebook"/>
-        </a>
-        <a
-          className="footer__social-link footer__social-link--github"
-          href="https://github.com/ubcuas"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={github} alt="Github"/>
-        </a>
-      </nav>
+      <p className="footer__legal-stuff">Legal stuff goes here</p>
+      <div className="footer__social-block">
+        <p className="footer__email">ubcuasemail@ubc.ca</p>
+        <nav className="footer__social" aria-label="Social links">
+          <a
+            className="footer__social-link footer__social-link--linkedin"
+            href="https://www.linkedin.com/company/ubcuas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="Linkedin"/>
+          </a>
+          <a
+            className="footer__social-link footer__social-link--instagram"
+            href="https://www.instagram.com/ubcuas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} alt="Instagram"/>
+          </a>
+          <a
+            className="footer__social-link footer__social-link--facebook"
+            href="https://www.facebook.com/ubcuas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="Facebook"/>
+          </a>
+          <a
+            className="footer__social-link footer__social-link--github"
+            href="https://github.com/ubcuas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} alt="Github"/>
+          </a>
+        </nav>
+      </div>
     </footer>
   )
 }
