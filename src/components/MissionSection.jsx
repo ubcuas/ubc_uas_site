153ec7fc70d64c6191120478297971cf80200e44
hixon1000 <harun.sa4@gmail.com>
2025-10-25 10:44:49 -0700

    Mineor changes to mission select, need fixing of background

--- src/components/MissionSection.jsx
+++ src/components/MissionSection.jsx
@@ -1,17 +1,8 @@
-import missionPhotoImg from '../../figma_ss/first page/third screen/image 19.png'
 import './MissionSection.css'
 
 const MissionSection = () => {
   return (
     <section className="mission" id="mission" aria-labelledby="mission-title">
-      <div className="mission__background" aria-hidden="true">
-        <img
-          src={missionPhotoImg}
-          alt=""
-          className="mission__background-image"
-          loading="lazy"
-        />
-      </div>
       <div className="mission__content">
         <h1 className="mission__title">OUR MISSION</h1>
         <text className="mission__statement">We are a multidisciplinary UBC design team that specializes in building autonomous drones and their support systems. We aim to push innovation and inspire the next generation of professionals.</text>
