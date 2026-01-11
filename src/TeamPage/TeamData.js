9b2e2a79392ea2d8bc2705a3bea99b41ad81abc0
Yash Vasdev <vasdevyash25@gmail.com>
2026-01-10 17:04:53 -0800

    fix

--- src/TeamPage/TeamData.js
+++ src/TeamPage/TeamData.js
@@ -1,4 +1,6 @@
 // TeamData.js
+// Uses imported URLs so your bundler (Vite/CRA/etc.) copies + hashes images for production.
+
 import captain from "../assets/teamPortraits/captain.png";
 import operationsLead from "../assets/teamPortraits/operationsLead.jpg";
 
@@ -25,54 +27,105 @@ export const teamData = [
     title: "Admin Team",
     theme: "charcoal",
     members: [
-      { heading: "Our Captain", name: "Rylan Stutters", img: captain, linkedinUrl: "https://www.linkedin.com/in/rylan-stutters-4b1a9a289/" },
-      { heading: "Operations", name: "Ethan Frketich", img: operationsLead, linkedinUrl: "https://www.linkedin.com/in/ethanfrk/" },
+      {
+        heading: "Our Captain",
+        name: "Rylan Stutters",
+        img: captain,
+        linkedinUrl: "https://www.linkedin.com/in/rylan-stutters-4b1a9a289/",
+      },
+      {
+        heading: "Operations",
+        name: "Ethan Frketich",
+        img: operationsLead,
+        linkedinUrl: "https://www.linkedin.com/in/ethanfrk/",
+      },
     ],
     groupImg: adminGroup,
-    caption: "Admin Team: ...",
+    caption:
+      "Admin Team: Vittal Ayer, Vasily Balabin, Aden Chan, Jaden Chan, Jonathan Chin, Ethan Frketich, Diego Islas, Ram Jayakumar, Leon Jiang, Manorama Joshi, Sean Lee, Olivia Lew, Amy Li, Yash Mali, Mercury McIndoe, Logan Neufield, Matthew Pirayesh, Rylan Stutters, Wayne Sudanartha, Ryan Wang, Cohen Weston, Nicholas Yap, Jason Yuan",
   },
+
   {
     title: "Aircraft Team",
     subtitle: "Co-Leads",
     theme: "teal",
     members: [
-      { name: "Sean Lee", img: aircraftSean, linkedinUrl: "https://www.linkedin.com/in/seanhlee411/" },
-      { name: "Vasily Balabin", img: aircraftVasily, linkedinUrl: "https://www.linkedin.com/in/vasily-balabin-32ab562a1/" },
+      {
+        name: "Sean Lee",
+        img: aircraftSean,
+        linkedinUrl: "https://www.linkedin.com/in/seanhlee411/",
+      },
+      {
+        name: "Vasily Balabin",
+        img: aircraftVasily,
+        linkedinUrl: "https://www.linkedin.com/in/vasily-balabin-32ab562a1/",
+      },
     ],
     groupImg: aircraftGroup,
-    caption: "Aircraft Team: ...",
+    caption:
+      "Aircraft Team: Vasily Balabin, Rowan Campbell, Grace Dauer, Marek Gryszka, Hyunmyung Lee, Kevin Ma, Arman Mukhi, Matthew Pirayesh, Anshul Raj, Ryan Wang, Nicholas Yap",
   },
+
   {
     title: "Payload Team",
     subtitle: "Co-Leads",
     theme: "charcoal",
     members: [
-      { name: "Jaden Chan", img: payloadJaden, linkedinUrl: "https://www.linkedin.com/in/jaden-chan-748856216/" },
-      { name: "Jason Yuan", img: payloadJason, linkedinUrl: "https://www.linkedin.com/in/jyuan10/" },
+      {
+        name: "Jaden Chan",
+        img: payloadJaden,
+        linkedinUrl: "https://www.linkedin.com/in/jaden-chan-748856216/",
+      },
+      {
+        name: "Jason Yuan",
+        img: payloadJason,
+        linkedinUrl: "https://www.linkedin.com/in/jyuan10/",
+      },
     ],
     groupImg: payloadGroup,
-    caption: "Payload Team: ...",
+    caption:
+      "Payload Team: Jaden Chan, Jonathan Chin, Yik Chun Choi, Dwight Dela Torre, Ryan Fan, Daniel Jin, Manorama Joshi, Inaya Merchant, Logan Neufield, Annie Sun, Jason Yuan",
   },
+
   {
     title: "Electrical Team",
     subtitle: "Co-Leads",
     theme: "teal",
     members: [
-      { name: "Wayne Sudanartha", img: electricalWayne, linkedinUrl: "https://www.linkedin.com/in/waynesdn/" },
-      { name: "Cohen Weston", img: electricalCohen, linkedinUrl: "https://www.linkedin.com/in/cohen-weston/" },
+      {
+        name: "Wayne Sudanartha",
+        img: electricalWayne,
+        linkedinUrl: "https://www.linkedin.com/in/waynesdn/",
+      },
+      {
+        name: "Cohen Weston",
+        img: electricalCohen,
+        linkedinUrl: "https://www.linkedin.com/in/cohen-weston/",
+      },
     ],
     groupImg: electricalGroup,
-    caption: "Electrical Team: ...",
+    caption:
+      "Electrical Team: Ethan Frketich, Kelly He, Ian Lee, Olivia Lew, Amy Li, Wayne Sudanartha, Cohen Weston",
   },
+
   {
     title: "Software Team",
     subtitle: "Co-Leads",
     theme: "charcoal",
     members: [
-      { name: "Mercury Mcindoe", img: softwareMercury, linkedinUrl: "https://www.linkedin.com/in/maplesyruphg06/" },
-      { name: "Ram Jayakumar", img: softwareRam, linkedinUrl: "https://www.linkedin.com/in/ram-jayakumar-2a096420b/" },
+      {
+        name: "Mercury Mcindoe",
+        img: softwareMercury,
+        linkedinUrl: "https://www.linkedin.com/in/maplesyruphg06/",
+      },
+      {
+        name: "Ram Jayakumar",
+        img: softwareRam,
+        linkedinUrl: "https://www.linkedin.com/in/ram-jayakumar-2a096420b/",
+      },
     ],
     groupImg: softwareGroup,
-    caption: "Software Team: ...",
+    caption:
+      "Software Team: Vittal Ayer, Chaitanya Choudhary, Wilfred Chow, Jingyang Cui, Michael Dickinson, Justin Hoang, Diego Islas, Ram Jayakumar, Anant Khanna, JunHyun Kim, Louis Lin, Anthony Lu, Yash Mali, Mercury McIndoe, Aniket Nemade, Minori Poedjokerto, Sankalp Prashanth, Harun Sameer, Athalia Setiawan, Sayyam Singla, Jason Staker, Yash Vasdev, Elsa Zheng",
   },
 ];
--- src/TeamPage/TeamPage.jsx
+++ src/TeamPage/TeamPage.jsx
@@ -1,51 +1,59 @@
+// TeamPage.jsx
 import { useEffect } from "react";
 import "./TeamPage.css";
 import ImageCarousel from "./ImageCarousel";
 import TeamSection from "./TeamSection";
 import { teamData } from "./TeamData";
 
+// CHANGED: import carousel images (so build rewrites URLs)
+import carousel1 from "../assets/carousel/teamCarousel/carousel1.jpg";
+import carousel2 from "../assets/carousel/teamCarousel/carousel2.jpg";
+import carousel3 from "../assets/carousel/teamCarousel/carousel3.png";
+import carousel4 from "../assets/carousel/teamCarousel/carousel4.jpg";
+import carousel5 from "../assets/carousel/teamCarousel/carousel5.jpg";
+import carousel6 from "../assets/carousel/teamCarousel/carousel6.jpg";
+import carousel7 from "../assets/carousel/teamCarousel/carousel7.jpg";
 
 export default function TeamPage() {
   // Separate images specifically for the header carousel
   const carouselImages = [
-    {
-      src: "src/assets/carousel/teamCarousel/carousel1.jpg",
-    },
-    { src: "src/assets/carousel/teamCarousel/carousel2.jpg", }, 
-    { src: "src/assets/carousel/teamCarousel/carousel3.png", },
-    { src: "src/assets/carousel/teamCarousel/carousel4.jpg", },
-    { src: "src/assets/carousel/teamCarousel/carousel5.jpg", },
-    { src: "src/assets/carousel/teamCarousel/carousel6.jpg", },
-    { src: "src/assets/carousel/teamCarousel/carousel7.jpg", },
+    { src: carousel1 },
+    { src: carousel2 },
+    { src: carousel3 },
+    { src: carousel4 },
+    { src: carousel5 },
+    { src: carousel6 },
+    { src: carousel7 },
+  ];
 
-  ]; // scroll-linked animation (parallax)
- useEffect(() => {
-  let latestY = window.scrollY || window.pageYOffset;
-  let ticking = false;
+  // scroll-linked animation (parallax)
+  useEffect(() => {
+    let latestY = window.scrollY || window.pageYOffset;
+    let ticking = false;
 
-  const updateScrollVar = () => {
-    document.documentElement.style.setProperty("--team-scroll", `${latestY}px`);
-    ticking = false;
-  };
+    const updateScrollVar = () => {
+      document.documentElement.style.setProperty("--team-scroll", `${latestY}px`);
+      ticking = false;
+    };
 
-  const handleScroll = () => {
-    latestY = window.scrollY || window.pageYOffset;
+    const handleScroll = () => {
+      latestY = window.scrollY || window.pageYOffset;
 
-    if (!ticking) {
-      window.requestAnimationFrame(updateScrollVar);
-      ticking = true;
-    }
-  };
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollVar);
+        ticking = true;
+      }
+    };
 
-  // initial value
-  handleScroll();
+    // initial value
+    handleScroll();
 
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-       <section className="team-page" aria-label="UBC UAS Team">
+    <section className="team-page" aria-label="UBC UAS Team">
       <div className="team-page__title-banner" aria-hidden="true" />
       <div className="team-heading-wrap">
         <h1 className="team-heading">Meet Our Team</h1>
